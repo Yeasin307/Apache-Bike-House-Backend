ba8b81f612203f7ea9691e0e3db12baf82cee5af
Yeasin <yeasinhossen7972@gmail.com>
2021-11-19 01:11:28 +0600

    final

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+async function run() {
+    try {
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello Doctors portal!')
+})
+
+app.listen(port, () => {
+    console.log(`listening at ${port}`)
+})
