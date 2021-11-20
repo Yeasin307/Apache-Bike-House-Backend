8d1d3f1bf84c588f015fa7ee4bf6b8e577ae8035
Yeasin <yeasinhossen7972@gmail.com>
2021-11-21 00:02:51 +0600

    final

--- index.js
+++ index.js
@@ -52,6 +52,17 @@ async function run() {
             res.json(result);
         });
 
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+
         app.post('/parchase', async (req, res) => {
             const newOrder = req.body;
             const result = await orderCollection.insertOne(newOrder);
