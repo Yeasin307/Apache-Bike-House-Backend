4ad76abfa7a11be42197d0f9c836e2ac212a8524
Yeasin <yeasinhossen7972@gmail.com>
2022-01-29 03:04:12 +0600

    Clean up ignored files

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lcr1a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
