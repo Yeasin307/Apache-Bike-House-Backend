39858628019f3723b1063d941cb1edb192a9b336
Yeasin <yeasinhossen7972@gmail.com>
2022-02-06 16:07:17 +0600

    added Firebase sdk

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-var serviceAccount = require('./apache-bike-house-firebase-adminsdk-tse11-7520a0a8dc.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
