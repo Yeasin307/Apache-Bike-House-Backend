19991a478ce12aa4e25dffbe69a53a4c39a72139
Yeasin <yeasinhossen7972@gmail.com>
2022-02-26 00:30:37 +0600

    add manage products

--- index.js
+++ index.js
@@ -193,12 +193,12 @@ async function run() {
             res.json(result);
         })
 
-        app.delete('/deleteproduct/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await productCollection.deleteOne(query);
-            res.json(result);
-        })
+        // app.delete('/deleteproduct/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const result = await productCollection.deleteOne(query);
+        //     res.json(result);
+        // })
 
     }
     finally {
